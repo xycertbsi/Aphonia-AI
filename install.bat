8924bb748180b419b894bfc644dd3e8f2af68f1b
XYCert <107841184+xycertbsi@users.noreply.github.com>
2024-10-02 22:08:48 +0200

    Windows telepítés

--- install.bat
+++ install.bat
@@ -1,7 +1,26 @@
-echo off
+@echo off
 
-echo PIP Csomagok telepitese...
-py -m pip install -r requertments.txt
+echo PIP csomagok telepitese...
+if not exist requirements.txt (
+    echo A requirements.txt fajl nem talalhato.
+    timeout /t 5
+    exit /b 1
+)
 
-echo Program futtatasa...
+py -m pip install -r requirements.txt
+if errorlevel 1 (
+    echo A csomagok telepitese nem sikerult.
+    timeout /t 5
+    exit /b 1
+)
+
+echo Program futtatas...
 py main.py
+if errorlevel 1 (
+    echo A program futtatasa nem sikerult.
+    timeout /t 5
+    exit /b 1
+)
+
+echo Minden folyamat sikeresen befejezodott.
+timeout /t 5
